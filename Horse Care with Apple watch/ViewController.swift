232009ce0068586ce6eff52ce52856cf88edc1ac
Jose Ramon Soto <student@meme-macbook.local>
2017-01-09 09:50:16 -0600

    Initial Commit

---
+++ Horse Care with Apple watch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Horse Care with Apple watch
+//
+//  Created by Jose Ramon Soto on 1/6/17.
+//  Copyright © 2017 Jose Ramon Soto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
